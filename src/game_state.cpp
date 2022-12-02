820211a1389e52573eaaef313d84a7cd508e95e5
Alexandr <jhmnn@yandex.ru>
2022-12-02 17:31:50 +0700

    Implement game state class

---
+++ src/game_state.cpp
@@ -0,0 +1,40 @@
+#include "game_state.hpp"
+
+#include "resources.hpp"
+
+namespace bruh {
+game_state::game_state(game_data_ptr _data) : data(_data) {}
+
+void game_state::init() {
+    srand(time(NULL));
+
+    map.load_level(GAME_LVL_FILEPATH);
+
+    sf::View view;
+    view.setCenter(sf::Vector2f((float)(map.get_level_size().x * map.get_tile_size().x) / 2.0f,
+                                (float)(map.get_level_size().y * map.get_tile_size().y) / 2.0f));
+    view.setSize(data->window.getSize().x, data->window.getSize().y);
+
+    data->window.setView(view);
+}
+
+void game_state::handle_input() {
+    sf::Event e;
+
+    while (data->window.pollEvent(e)) {
+        if (e.type == sf::Event::Closed) {
+            data->window.close();
+        }
+    }
+}
+
+void game_state::update(float dt) {}
+
+void game_state::draw(float dt) {
+    data->window.clear(sf::Color(0, 5, 25));
+
+    map.draw(data->window);
+
+    data->window.display();
+}
+}  // namespace bruh
---
+++ src/game_state.hpp
@@ -0,0 +1,27 @@
+#ifndef GAME_STATE_HPP
+#define GAME_STATE_HPP
+
+#include "game.hpp"
+#include "level.hpp"
+#include "state.hpp"
+
+namespace bruh {
+class game_state : public state {
+   private:
+    game_data_ptr data;
+    sf::Clock clock;
+
+    level map;
+
+   public:
+    game_state(game_data_ptr _data);
+
+    void init();
+
+    void handle_input();
+    void update(float dt);
+    void draw(float dt);
+};
+}  // namespace bruh
+
+#endif  // GAME_STATE_HPP
