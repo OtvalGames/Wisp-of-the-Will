387821ed717592e90179cb9e033e351617fa0c67
meequrox <117393211+meequrox@users.noreply.github.com>
2022-12-03 01:42:17 +0700

    Implement main menu state

---
+++ src/main_menu_state.hpp
@@ -0,0 +1,29 @@
+#ifndef MAIN_MENU_STATE_HPP
+#define MAIN_MENU_STATE_HPP
+
+#include "game.hpp"
+#include "state.hpp"
+
+namespace yapg {
+enum buttons { play = 0, exit };
+
+class main_menu_state : public state {
+   private:
+    game_data_ptr data;
+    sf::Clock clock;
+
+    sf::Sprite bg;
+    std::deque<sf::Sprite*> buttons;
+
+   public:
+    main_menu_state(game_data_ptr _data);
+
+    void init();
+
+    void handle_input();
+    void update(float dt);
+    void draw(float dt);
+};
+}  // namespace yapg
+
+#endif  // MAIN_MENU_STATE_HPP
