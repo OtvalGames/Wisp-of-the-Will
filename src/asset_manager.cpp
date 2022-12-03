b35b16fa49c8e15341052cfa4c5280b3d4014b06
meequrox <117393211+meequrox@users.noreply.github.com>
2022-12-03 23:59:28 +0700

    Asset manager: remove unused load_texture() overload

--- src/asset_manager.cpp
+++ src/asset_manager.cpp
@@ -7,11 +7,5 @@ void asset_manager::load_texture(std::string _name, std::string _file_name) {
     if (txtr.loadFromFile(_file_name)) textures[_name] = txtr;
 }
 
-void asset_manager::load_texture(std::string _name, std::string _file_name, sf::IntRect _texture_rect) {
-    sf::Texture txtr;
-
-    if (txtr.loadFromFile(_file_name, _texture_rect)) textures[_name] = txtr;
-}
-
 sf::Texture& asset_manager::get_texture(std::string _name) { return textures.at(_name); }
 }  // namespace yapg
