c9536c73542f855e9208f69e9842f0a186c8e4ee
meequrox <117393211+meequrox@users.noreply.github.com>
2022-12-06 22:05:18 +0700

    Minor window name corrections

--- src/game.cpp
+++ src/game.cpp
@@ -7,10 +7,11 @@ game::game() {
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
-    data->window.create(sf::VideoMode().getFullscreenModes().at(0), "YAPG",
-    sf::Style::Fullscreen, settings);
-    // data->window.create(sf::VideoMode().getFullscreenModes().at(2), "Wisp of the Will", sf::Style::Default,
-                        // settings);
+    // data->window.create(sf::VideoMode().getFullscreenModes().at(2), "Wisp of the Will",
+    // sf::Style::Default, settings);
+    data->window.create(sf::VideoMode().getFullscreenModes().at(0), "Wisp of the Will",
+                        sf::Style::Fullscreen, settings);
+
     data->window.setFramerateLimit(framerate);
     data->window.setPosition(sf::Vector2i(0, 0));
     data->window.setKeyRepeatEnabled(false);
